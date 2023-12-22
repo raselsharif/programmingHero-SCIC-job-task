3401d2c5be09689c17a46b37e2ac4dd40d6758b5
raselsharif <raselsharif9@gmail.com>
2023-12-22 16:17:52 +0600

    added post api for post task and implement reat hook form in task input

--- src/pages/Dashboard/CreatTask/CreateTask.jsx
+++ src/pages/Dashboard/CreatTask/CreateTask.jsx
@@ -1,20 +1,93 @@
-import { Button, Label, TextInput, Textarea } from "flowbite-react";
+import axios from "axios";
+import { Button, Label, Select, TextInput, Textarea } from "flowbite-react";
 import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
 
 const CreateTask = () => {
-  const handleCreate = (e) => {
-    e.preventDefault();
-    toast.success("clicked successfully");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const onSubmit = (data) => {
+    const task = {
+      ...data,
+      createDate: Date(),
+    };
+    console.log(task);
+    axios
+      .post("http://localhost:5000/create-task", task)
+      .then((res) => {
+        if (res.data.acknowledged) {
+          toast.success("Task Created Successfully");
+          reset();
+        }
+      })
+      .then((err) => {
+        if (err) {
+          toast.error("Task Not Created");
+        }
+      });
   };
   return (
     <div className="max-w-sm mx-auto mt-20">
-      <form onSubmit={handleCreate}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Label color="black" value="Task Title" />
-        <TextInput className="my-3" placeholder="Task Title" />
-        <Label color="black" value="Deadline" />
-        <TextInput type="date" className="my-3" />
+        <TextInput
+          className="my-3"
+          placeholder="Task Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title?.type === "required" && (
+          <p className="text-red-500 mt-2" role="alert">
+            Title is required
+          </p>
+        )}
+        <div className="flex gap-4">
+          <div className="w-full">
+            <Label color="black" value="Deadline" />
+            <TextInput
+              type="date"
+              className="my-3"
+              {...register("deadLine", { required: true })}
+            />
+            {errors.deadLine?.type === "required" && (
+              <p className="text-red-500 mt-2" role="alert">
+                Deadline is required
+              </p>
+            )}
+          </div>
+          <div className="w-full">
+            <Label color="black" value="Priority" />
+            <Select
+              {...register("priority", { required: true })}
+              className="my-3"
+            >
+              <option>High</option>
+              <option>Moderator</option>
+              <option>Low</option>
+            </Select>
+
+            {errors.priority?.type === "required" && (
+              <p className="text-red-500 mt-2" role="alert">
+                Priority is required
+              </p>
+            )}
+          </div>
+        </div>
         <Label color="black" value="Description" />
-        <Textarea className="mt-3" placeholder="Task Details..." rows={8} />
+        <Textarea
+          className="mt-3"
+          placeholder="Task Details..."
+          rows={8}
+          {...register("des", { required: true })}
+        />
+        {errors.des?.type === "required" && (
+          <p className="text-red-500 mt-2" role="alert">
+            Details is required
+          </p>
+        )}
         <Button type="submit" className="mt-3">
           Create
         </Button>
