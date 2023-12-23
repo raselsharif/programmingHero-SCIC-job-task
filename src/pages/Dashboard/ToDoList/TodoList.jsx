351c241e3e7496dad69f4842b3efef4e9cbc53dc
raselsharif <raselsharif9@gmail.com>
2023-12-24 01:02:34 +0600

    added on going and completed section

--- src/pages/Dashboard/ToDoList/TodoList.jsx
+++ src/pages/Dashboard/ToDoList/TodoList.jsx
@@ -16,14 +16,22 @@ const TodoList = () => {
       });
   }, [user.email]);
   return (
-    <div className="mt-20 p-4">
-      <div>
+    <div className="mt-20 p-4 grid grid-cols-2 gap-3">
+      <div className="border p-1 border-cyan-600">
         <h3 className="text-center mb-4 text-cyan-600 text-2xl">To-do</h3>
         <div className="flex flex-col gap-3">
           {tasks?.map((task) => (
             <ToDoCard key={task._id} task={task} />
           ))}
         </div>
+      </div>
+      <div className="border p-1 border-cyan-600">
+        <h3 className="text-center mb-4 text-cyan-600 text-2xl">On Going</h3>
+        <div className="flex flex-col gap-3"></div>
+      </div>
+      <div className="border p-1 border-cyan-600">
+        <h3 className="text-center mb-4 text-cyan-600 text-2xl">Completed</h3>
+        <div className="flex flex-col gap-3"></div>
       </div>
     </div>
   );
